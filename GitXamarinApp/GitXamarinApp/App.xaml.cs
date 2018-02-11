471825b0f209c75110e0203f9b315f1f084bce5d
TimGeeTest <36365712+TimGeeTest@users.noreply.github.com>
2018-02-11 12:06:13 +0000

    Added VS App Centre code

--- GitXamarinApp/GitXamarinApp/App.xaml.cs
+++ GitXamarinApp/GitXamarinApp/App.xaml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
 using Xamarin.Forms;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace GitXamarinApp
 {
@@ -19,6 +21,7 @@ namespace GitXamarinApp
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("android=ce517679-1d54-42bb-85ef-9d4c532ba7f2;" + "uwp={Your UWP App secret here};" + "ios={Your iOS App secret here}", typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
